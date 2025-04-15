611c7fae0819a801069569aceb352902c9854b25
llalallalala <s2401231@edu.bc.fi>
2025-04-15 10:21:35 +0300

    Add files via upload

---
+++ Tehtävä 1: Tervehdys-komponentti/App.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Tervehdys from './tervehdys.jsx';
+
+function App() {
+  const s = "d";
+  console.log("aa")
+  return (
+    <div>
+      <Tervehdys nm="Henri"/>
+    </div>
+  )
+}
+
+export default App
---
+++ Tehtävä 1: Tervehdys-komponentti/tervehdys.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+function Tervehdys({nm}) {
+    return(
+        <p>Hei, {nm} !! :D</p>
+    )
+}
+
+export default Tervehdys;
